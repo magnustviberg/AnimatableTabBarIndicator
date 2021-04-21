7e1bf5262b8e0ebf5e0d3917401ef4d0d4718797
Magnus <hei@purpl.no>
2021-04-21 18:17:49 +0200

    Initial Commit

---
+++ CustomTabbarExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabbarExample
+//
+//  Created by Magnus Tviberg on 21/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
